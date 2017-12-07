fa34f12a7d42a946ce3b451f66456eafe0b3fef2
Caio Gondim <me@caiogondim.com>
2017-12-07 14:19:16 -0500

    Block event reference as literal values

--- test/index.js
+++ test/index.js
@@ -5,47 +5,62 @@ class Foo extends StrictEmitter {}
 
 // Behaves as native EventEmitter for known events
 ;(() => {
-  const foo = new Foo(['a'])
+  const foo = new Foo({ events: ['a'] })
   let count = 1
-  foo.on('a', () => count += 1)
-  foo.emit('a')
+  foo.on(foo.events.a, () => count += 1)
+  foo.emit(foo.events.a)
 
   assert.equal(count, 2)
 })()
 
-// Throws TypeError if a listener tries to listen for an undefined event
+// Throws TypeError if a listener tries to listen to an undefined event
 ;(() => {
-  const foo = new Foo(['a'])
+  const foo = new Foo({ events: ['a'] })
 
   assert.throws(
-    () => foo.on('b', () => {}),
+    () => foo.on(foo.events.b, () => {}),
     TypeError
   )
 })()
 
 // Throws TypeError if it tries to emit undefined event
 ;(() => {
-  const foo = new Foo(['a'])
+  const foo = new Foo({ events: ['a'] })
 
   assert.throws(
-    () => foo.emit('b'),
+    () => foo.emit(foo.events.b),
     TypeError
   )
 })()
 
 // Allows event to be added after instantiation
 ;(() => {
-  const foo = new Foo(['a'])
+  const foo = new Foo({ events: ['a'] })
 
   assert.throws(
-    () => foo.emit('b'),
+    () => foo.emit(foo.events.b),
     TypeError
   )
 
-  foo.events.push('b')
+  foo.addEvent('b')
 
   assert.doesNotThrow(
-    () => foo.emit('b'),
+    () => foo.emit(foo.events.b),
+    TypeError
+  )
+})()
+
+// Dont allow event to be referenced as literal value
+;(() => {
+  const foo = new Foo({ events: ['a'] })
+
+  assert.throws(
+    () => foo.emit('a'),
+    TypeError
+  )
+
+  assert.doesNotThrow(
+    () => foo.emit(foo.events.a),
     TypeError
   )
 })()
