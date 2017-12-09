6c7f669b80d3508fd26258e5609fc68a91650243
Caio Gondim <me@caiogondim.com>
2017-12-08 15:06:11 -1000

    Add lint task

--- test/events.js
+++ test/events.js
@@ -6,12 +6,12 @@ class Foo extends StrictEmitter {}
 test('dont allow direct modification to this.events', t => {
   const foo = new Foo({ events: ['a'] })
 
-  t.throws(() => foo.events.a = 1, TypeError)
-  t.throws(() => foo.events = undefined, TypeError)
+  t.throws(() => { foo.events.a = 1 }, TypeError)
+  t.throws(() => { foo.events = undefined }, TypeError)
 })
 
 test('uses Proxy only when available', t => {
   const foo = new Foo({ events: ['a'], __Proxy__: null })
 
-  t.notThrows(() => foo.events.a = '', TypeError)
+  t.notThrows(() => { foo.events.a = '' }, TypeError)
 })
--- test/on.js
+++ test/on.js
@@ -6,7 +6,7 @@ class Foo extends StrictEmitter {}
 test('behaves as native EventEmitter for known events', t => {
   const foo = new Foo({ events: ['a'] })
   let count = 1
-  foo.on(foo.events.a, () => count += 1)
+  foo.on(foo.events.a, () => { count += 1 })
   foo.emit(foo.events.a)
 
   t.is(count, 2)
